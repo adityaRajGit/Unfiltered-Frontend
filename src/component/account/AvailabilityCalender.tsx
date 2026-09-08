efd0434968219f3fba4f08a95092321c61388144
RishabhRawat2003 <rawatrishabh953@gmail.com>
2026-09-08 20:36:15 +0530

    feat: extend availability options in calendar component to include evening hours

--- src/component/account/AvailabilityCalender.tsx
+++ src/component/account/AvailabilityCalender.tsx
@@ -32,7 +32,8 @@ const timeOptions = [
     '08:00', '09:00', '10:00',
     '11:00', '12:00', '13:00',
     '14:00', '15:00', '16:00',
-    '17:00', '18:00', '19:00'
+    '17:00', '18:00', '19:00',
+    '20:00', '21:00', '22:00', '23:00'
 ];
 
 export default function CalendarAvailabilityScheduler({ therapistId }: { therapistId: string }) {
